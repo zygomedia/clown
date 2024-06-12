de4f4db73dfb5f638afedf9eef0ea545cfed68a1
Awpteamoose <awpteamoose@gmail.com>
2024-06-12 11:03:44 +0100

    a little uggy duplicated code for non-clone honks (aka slips)

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ use itertools::Itertools;
 #[derive(Default)]
 struct Clown {
 	honks: HashMap<syn::Ident, syn::Expr>,
+	slips: HashMap<syn::Ident, syn::Expr>,
 }
 
 impl Clown {
@@ -23,17 +24,31 @@ impl Clown {
 				},
 				TokenTree::Literal(_) | TokenTree::Punct(_) => acc.extend(once(tree)),
 				TokenTree::Ident(ref ident) => {
-					if ident != "honk" { acc.extend(once(tree)); continue; }
-					let Some(TokenTree::Punct(punct)) = tt_iter.peek() else { acc.extend(once(tree)); continue; };
-					if punct.as_char() != '!' { acc.extend(once(tree)); continue; }
-					let Some(TokenTree::Group(group)) = tt_iter.peek() else { acc.extend(once(tree)); continue; };
-					let expr = syn::parse2::<syn::Expr>(group.stream()).expect("honk! macro argument must be an expression");
-					let ident = syn::Ident::new(&format!("__honk_{}", self.honks.len()), expr.span());
-					acc.extend(once(TokenTree::Ident(ident.clone())));
-					self.honks.insert(ident, expr);
+					if ident == "honk" {
+						let Some(TokenTree::Punct(punct)) = tt_iter.peek() else { acc.extend(once(tree)); continue; };
+						if punct.as_char() != '!' { acc.extend(once(tree)); continue; }
+						let Some(TokenTree::Group(group)) = tt_iter.peek() else { acc.extend(once(tree)); continue; };
+						let expr = syn::parse2::<syn::Expr>(group.stream()).expect("honk! macro argument must be an expression");
+						let ident = syn::Ident::new(&format!("__honk_{}", self.honks.len()), expr.span());
+						acc.extend(once(TokenTree::Ident(ident.clone())));
+						self.honks.insert(ident, expr);
 
-					// we got `honk` as next, now skip `!` and group with args
-					tt_iter.next(); tt_iter.next();
+						// we got `honk` as next, now skip `!` and group with args
+						tt_iter.next(); tt_iter.next();
+					} else if ident == "slip" {
+						let Some(TokenTree::Punct(punct)) = tt_iter.peek() else { acc.extend(once(tree)); continue; };
+						if punct.as_char() != '!' { acc.extend(once(tree)); continue; }
+						let Some(TokenTree::Group(group)) = tt_iter.peek() else { acc.extend(once(tree)); continue; };
+						let expr = syn::parse2::<syn::Expr>(group.stream()).expect("slip! macro argument must be an expression");
+						let ident = syn::Ident::new(&format!("__slip_{}", self.slips.len()), expr.span());
+						acc.extend(once(TokenTree::Ident(ident.clone())));
+						self.slips.insert(ident, expr);
+
+						// we got `slip` as next, now skip `!` and group with args
+						tt_iter.next(); tt_iter.next();
+					} else {
+						acc.extend(once(tree));
+					}
 				},
 			}
 		}
@@ -50,11 +65,18 @@ impl syn::visit_mut::VisitMut for Clown {
 	fn visit_expr_mut(&mut self, this_expr: &mut syn::Expr) {
 		match this_expr {
 			syn::Expr::Macro(syn::ExprMacro { mac, .. }) => {
-				if !mac.path.get_ident().is_some_and(|x| x == "honk") { return syn::visit_mut::visit_expr_mut(self, this_expr); }
-				let expr = syn::parse2::<syn::Expr>(mac.tokens.clone()).expect("honk! macro argument must be an expression");
-				let ident = syn::Ident::new(&format!("__honk_{}", self.honks.len()), this_expr.span());
-				*this_expr = syn::parse_quote!(#ident);
-				self.honks.insert(ident, expr);
+				let Some(mac_ident) = mac.path.get_ident() else { return syn::visit_mut::visit_expr_mut(self, this_expr); };
+				if mac_ident == "honk" {
+					let expr = syn::parse2::<syn::Expr>(mac.tokens.clone()).expect("honk! macro argument must be an expression");
+					let ident = syn::Ident::new(&format!("__honk_{}", self.honks.len()), this_expr.span());
+					*this_expr = syn::parse_quote!(#ident);
+					self.honks.insert(ident, expr);
+				} else if mac_ident == "slip" {
+					let expr = syn::parse2::<syn::Expr>(mac.tokens.clone()).expect("slip! macro argument must be an expression");
+					let ident = syn::Ident::new(&format!("__slip_{}", self.slips.len()), this_expr.span());
+					*this_expr = syn::parse_quote!(#ident);
+					self.slips.insert(ident, expr);
+				}
 			},
 			// do not recurse into closures in case there's a nested clown
 			syn::Expr::Closure(_) => {},
@@ -72,10 +94,12 @@ pub fn clown(_: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_
 	clown.visit_expr_closure_mut(&mut item);
 
 	let honks = clown.honks.into_iter().map(|(ident, expr)| { let span = expr.span(); quote_spanned! {span=> let #ident = (#expr).clone();} });
+	let slips = clown.slips.into_iter().map(|(ident, expr)| { let span = expr.span(); quote_spanned! {span=> let #ident = #expr;} });
 
 	quote! {
 		{
 			#(#honks)*
+			#(#slips)*
 			#item
 		}
 	}.into()
