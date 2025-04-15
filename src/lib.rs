208ce8894fefe3585d2490f5073b4b5912414b52
Awpteamoose <awpteamoose@gmail.com>
2025-04-15 12:39:20 +0100

    just define honk! and slip! as noops to satisfy rust-analyzer

--- src/lib.rs
+++ src/lib.rs
@@ -101,3 +101,6 @@ pub fn clown(_: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_
 		#item
 	}}.into()
 }
+
+#[proc_macro] pub fn honk(item: proc_macro::TokenStream) -> proc_macro::TokenStream { item }
+#[proc_macro] pub fn slip(item: proc_macro::TokenStream) -> proc_macro::TokenStream { item }
